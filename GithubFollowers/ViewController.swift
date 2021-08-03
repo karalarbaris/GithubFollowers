9a019f3f1bd97080845080f9f3d43bde128e018d
Baris <karalarbaris@gmail.com>
2021-08-03 14:34:23 +0200

    3 Initial Project Setup  No Storyboard

--- GithubFollowers/ViewController.swift
+++ GithubFollowers/ViewController.swift
@@ -12,6 +12,7 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        view.backgroundColor = #colorLiteral(red: 0.9254902005, green: 0.2352941185, blue: 0.1019607857, alpha: 1)
     }
 
 
