537302ea64e47a004820638b121c72ef0f34a7c2
Baris <karalarbaris@gmail.com>
2021-08-09 16:23:59 +0200

    10 Planning our Network Calls API JSON Model

---
+++ GithubFollowers/Model/Follower.swift
@@ -0,0 +1,13 @@
+//
+//  Follower.swift
+//  GithubFollowers
+//
+//  Created by Baris Karalar on 9.08.2021.
+//
+
+import Foundation
+
+struct Follower: Codable {
+    var login: String
+    var avatarUrl: String
+}
