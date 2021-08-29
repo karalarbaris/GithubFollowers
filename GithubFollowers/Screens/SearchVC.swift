d263b2d5611820eac1c24a6bf93767d9522d4fe3
Baris <karalarbaris@gmail.com>
2021-08-29 16:34:57 +0200

    40 - Constraint fix

--- GithubFollowers/Screens/SearchVC.swift
+++ GithubFollowers/Screens/SearchVC.swift
@@ -62,10 +62,9 @@ class SearchVC: UIViewController {
         logoImageView.image = Constants.Images.bkLogo
         
         let topConstraintConstant: CGFloat = Constants.DeviceTypes.isiPhoneSE || Constants.DeviceTypes.isiPhone8Zoomed ? 20 : 80
-        logoImageViewTopConstraint = logoImageView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: topConstraintConstant)
-        logoImageViewTopConstraint.isActive = true
         
         NSLayoutConstraint.activate([
+            logoImageView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: topConstraintConstant),
             logoImageView.centerXAnchor.constraint(equalTo: view.centerXAnchor),
             logoImageView.heightAnchor.constraint(equalToConstant: 200),
             logoImageView.widthAnchor.constraint(equalToConstant: 200)
